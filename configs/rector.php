2dc5ff73f737c092223703bf1c3dec6803810858
Andrea Falco <andrea@falco.sh>
2022-07-10 11:53:31 +0200

    Applied Doctrine coding standard

--- configs/rector.php
+++ configs/rector.php
@@ -1,23 +1,35 @@
 <?php
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\ClassMethod\DateTimeToDateTimeInterfaceRector;
+use Rector\CodeQuality\Rector\Equal\UseIdenticalOverEqualWithSameTypeRector;
+use Rector\CodeQuality\Rector\If_\ShortenElseIfRector;
 use Rector\Config\RectorConfig;
 use Rector\Core\Configuration\Option;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector;
+use Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector;
+use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
+use Rector\Naming\Rector\ClassMethod\RenameVariableToMatchNewTypeRector;
+use Rector\Naming\Rector\Foreach_\RenameForeachValueVariableToMatchExprVariableRector;
+use Rector\Naming\Rector\Foreach_\RenameForeachValueVariableToMatchMethodCallReturnTypeRector;
+use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Privatization\Rector\Class_\ChangeReadOnlyVariableWithDefaultValueToConstantRector;
+use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
+use Rector\Privatization\Rector\Class_\RepeatedLiteralToClassConstantRector;
 use Rector\Set\ValueObject\SetList;
 
 // https://github.com/rectorphp/rector/blob/main/docs/rector_rules_overview.md
 
-return static function (RectorConfig $rectorConfig): void
-{
+return static function (RectorConfig $rectorConfig): void {
     $searchPaths = [
-        __DIR__.'/bin',
-        __DIR__.'/public',
-        __DIR__.'/src',
-        __DIR__.'/tests',
+        __DIR__ . '/bin',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
     ];
     $paths = array_merge([
-        __DIR__.'/rector.php',
+        __DIR__ . '/rector.php',
     ], array_filter($searchPaths, 'file_exists'));
 
     $rectorConfig->sets([
@@ -36,23 +48,23 @@ return static function (RectorConfig $rectorConfig): void
     ]);
     $rectorConfig->parallel();
     $rectorConfig->paths($paths);
-    $rectorConfig->phpstanConfig(__DIR__.'/phpstan.neon.dist');
+    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon.dist');
     $rectorConfig->skip([
-        \Rector\CodeQuality\Rector\ClassMethod\DateTimeToDateTimeInterfaceRector::class,
-        \Rector\CodeQuality\Rector\Equal\UseIdenticalOverEqualWithSameTypeRector::class,
-        \Rector\CodeQuality\Rector\If_\ShortenElseIfRector::class,
-        \Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector::class,
-        \Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector::class,
-        \Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector::class,
-        \Rector\Naming\Rector\ClassMethod\RenameVariableToMatchNewTypeRector::class,
-        \Rector\Naming\Rector\Foreach_\RenameForeachValueVariableToMatchExprVariableRector::class,
-        \Rector\Naming\Rector\Foreach_\RenameForeachValueVariableToMatchMethodCallReturnTypeRector::class,
-        \Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector::class,
-        \Rector\Privatization\Rector\Class_\ChangeReadOnlyVariableWithDefaultValueToConstantRector::class,
-        \Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector::class,
-        \Rector\Privatization\Rector\Class_\RepeatedLiteralToClassConstantRector::class,
+        DateTimeToDateTimeInterfaceRector::class,
+        UseIdenticalOverEqualWithSameTypeRector::class,
+        ShortenElseIfRector::class,
+        RemoveUnusedPromotedPropertyRector::class,
+        RenameVariableToMatchMethodCallReturnTypeRector::class,
+        RenameParamToMatchTypeRector::class,
+        RenameVariableToMatchNewTypeRector::class,
+        RenameForeachValueVariableToMatchExprVariableRector::class,
+        RenameForeachValueVariableToMatchMethodCallReturnTypeRector::class,
+        AddSeeTestAnnotationRector::class,
+        ChangeReadOnlyVariableWithDefaultValueToConstantRector::class,
+        FinalizeClassesWithoutChildrenRector::class,
+        RepeatedLiteralToClassConstantRector::class,
     ]);
 
     $parameters = $rectorConfig->parameters();
-    $parameters->set(Option::CACHE_DIR, __DIR__.'/build/cache/rector');
+    $parameters->set(Option::CACHE_DIR, __DIR__ . '/build/cache/rector');
 };
